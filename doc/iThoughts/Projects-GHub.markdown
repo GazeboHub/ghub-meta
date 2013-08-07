28be77b36102367a24a80a1600b701fc6cc80e89
Sean Champ <spchamp+git-muletop1@me.com>
2013-08-07 16:51:15 -0700

    adding documentation about GHub-Meta project and its predecssor GHub

---
+++ doc/iThoughts/Projects-GHub.markdown
@@ -0,0 +1,28 @@
+
+# Gazebo Hub
+
+----  
+  _/Gazebo Hub_
+
+
+## Projects
+
+* GHub-Meta
+    * New
+    * "Administrative Core Project"
+    * Original "home" of this outline
+    * Original Premise: Central organization hub for component projects
+* GHub
+    * Old
+    * Hosted originally at SourceForge.net
+    * Original Premise: "Extending enterprise portal systems for community assistance"
+        * Contrast to
+            * Complexity of enterprise portal frameworks
+            * "Summer 2013"
+                * No opportunity for actively pursuing community support of the project
+                * No opportunity for further development of project goals
+            * Community unfamiliarity with regards to trends and practices in social entrepreneurialism
+    * Later Premise: Central organization hub for component projects
+
+## Upstream Projects
+
---
+++ doc/iThoughts/Projects-GHub.mm
@@ -0,0 +1,22 @@
+<map version="0.9.0"><attribute_registry SHOW_ATTRIBUTES="hide"/>
+<node TEXT="Gazebo Hub" ID="Freemind_Link_1"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Projects" ID="Freemind_Link_2"  POSITION="right"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="GHub-Meta" ID="Freemind_Link_3"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><icon BUILTIN="go"/>
+<node TEXT="New" ID="Freemind_Link_6"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="&quot;Administrative Core Project&quot;" ID="Freemind_Link_7"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Original &quot;home&quot; of this outline" ID="Freemind_Link_8"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Original Premise: Central organization hub for component projects" ID="Freemind_Link_24"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="GHub" ID="Freemind_Link_9"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><arrowlink COLOR="#477DB2" DESTINATION="Freemind_Link_3" ENDARROW="Default" ENDINCLINATION="22;0;" ID="Arrow_ID_29" STARTARROW="None" STARTINCLINATION="22;0;" SOURCE_LABEL="" MIDDLE_LABEL="Reorganize As" TARGET_LABEL="" /><node TEXT="Old" ID="Freemind_Link_10"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Hosted originally at SourceForge.net" ID="Freemind_Link_11"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Original Premise: &quot;Extending enterprise portal systems for community assistance&quot;" ID="Freemind_Link_12"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Contrast to" ID="Freemind_Link_14"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><arrowlink COLOR="#477DB2" DESTINATION="Freemind_Link_13" ENDARROW="Default" ENDINCLINATION="22;0;" ID="Arrow_ID_30" STARTARROW="None" STARTINCLINATION="22;0;" SOURCE_LABEL="" MIDDLE_LABEL="After consideration" TARGET_LABEL="" /><node TEXT="Complexity of enterprise portal frameworks" ID="Freemind_Link_15"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="&quot;Summer 2013&quot;" ID="Freemind_Link_18"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="No opportunity for actively pursuing community support of the project" ID="Freemind_Link_16"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="No opportunity for further development of project goals" ID="Freemind_Link_17"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="Community unfamiliarity with regards to trends and practices in social entrepreneurialism" ID="Freemind_Link_19"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><arrowlink COLOR="#477DB2" DESTINATION="Freemind_Link_16" ENDARROW="Default" ENDINCLINATION="22;0;" ID="Arrow_ID_31" STARTARROW="None" STARTINCLINATION="22;0;" SOURCE_LABEL="" MIDDLE_LABEL="See also" TARGET_LABEL="" /><arrowlink COLOR="#477DB2" DESTINATION="Freemind_Link_17" ENDARROW="Default" ENDINCLINATION="22;0;" ID="Arrow_ID_32" STARTARROW="None" STARTINCLINATION="22;0;" SOURCE_LABEL="" MIDDLE_LABEL="See also" TARGET_LABEL="" /></node>
+</node>
+</node>
+<node TEXT="Later Premise: Central organization hub for component projects" ID="Freemind_Link_13"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+</node>
+<node TEXT="Upstream Projects" ID="Freemind_Link_4"  POSITION="right"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+</map>
